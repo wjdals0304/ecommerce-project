ce32e811f91a78439b78793ae43dd0a429fbbd97
JungMin <wjdals0304@gmail.com>
2025-01-23 20:01:00 +0900

    feat: intent 수정

--- src/features/home/components/HomeCategory.jsx
+++ src/features/home/components/HomeCategory.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import "./homeCategory.css";
+import React from 'react';
+import './homeCategory.css';
 
 function HomeCategory() {
-    return ( 
-       <ul class="categories"> 
-            <li class="category-item">
-                <img src="" alt="Gaming"></img>
-                <span>Gaming</span>
-            </li>
-            <li class="category-item">
-                <img src="" alt="Smartphone"></img>
-                <span>Smartphone</span>
-            </li>
-            <li class="category-item">
-                <img src="" alt="Laptop"></img>
-                <span>Laptop</span>
-            </li>
-            <li class="category-item">
-                <img src="" alt="TV"></img>
-                <span>TV</span>
-            </li>
-            <li class="category-item">
-                <img src="" alt="Camera"></img>
-                <span>Camera</span>
-            </li>
-            <li class="category-item">
-                <img src="" alt="More"></img>
-                <span>More</span>
-            </li>
-       </ul>
-    )
+  return (
+    <ul class="categories">
+      <li class="category-item">
+        <img src="" alt="Gaming"></img>
+        <span>Gaming</span>
+      </li>
+      <li class="category-item">
+        <img src="" alt="Smartphone"></img>
+        <span>Smartphone</span>
+      </li>
+      <li class="category-item">
+        <img src="" alt="Laptop"></img>
+        <span>Laptop</span>
+      </li>
+      <li class="category-item">
+        <img src="" alt="TV"></img>
+        <span>TV</span>
+      </li>
+      <li class="category-item">
+        <img src="" alt="Camera"></img>
+        <span>Camera</span>
+      </li>
+      <li class="category-item">
+        <img src="" alt="More"></img>
+        <span>More</span>
+      </li>
+    </ul>
+  );
 }
 
 export default HomeCategory;
