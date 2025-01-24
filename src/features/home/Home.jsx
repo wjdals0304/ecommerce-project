2f869699717f0f45310c5e0e32abf8e833b08139
JungMin <wjdals0304@gmail.com>
2025-01-24 14:17:02 +0900

    feat: 베스트셀러 UI 추가

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,17 +1,20 @@
-import Search from "./Search"
-import HomeHeader from "./components/HomeHeader";
-import HomeCategory from "./components/HomeCategory";
-import HomeProme from "./components/HomePromo";
+import Search from './Search';
+import HomeHeader from './components/HomeHeader';
+import HomeCategory from './components/HomeCategory';
+import HomeProme from './components/HomePromo';
+import HomeBestSeller from './components/HomeBestSeller';
+import './home.css';
 
 function Home() {
-    return ( 
-        <div>
-            <Search />  
-            <HomeHeader />     
-            <HomeCategory /> 
-            <HomeProme />
-        </div>
-    )
+  return (
+    <div className="home-container">
+      <Search />
+      <HomeHeader />
+      <HomeCategory />
+      <HomeProme />
+      <HomeBestSeller />
+    </div>
+  );
 }
 
 export default Home;
