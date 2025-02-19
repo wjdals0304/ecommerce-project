fc3295302be9019cbd65682f6ca05897f264f16b
JungMin <wjdals0304@gmail.com>
2025-02-19 09:59:15 +0900

    feat: 리뷰 반영 - useController, useFormContext 사용

--- src/features/cart/cartProcess/shoppingCheckOut/ShoppingCheckOut.tsx
+++ src/features/cart/cartProcess/shoppingCheckOut/ShoppingCheckOut.tsx
@@ -1,8 +1,10 @@
-import {useForm} from 'react-hook-form';
+import {Resolver, useForm, FormProvider} from 'react-hook-form';
 import styled from 'styled-components';
 import {CartResponse} from '@/types/cart';
 import {postRequest} from '@/utils/apiClient';
 import {API_ENDPOINTS} from '@/config/ApiEndPoints';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
 import ShoppingCheckOutOrder from '../shoppingCheckOut/ShoppingCheckOutOrder';
 import {useState} from 'react';
 import ShoppingCheckOutInfo from '../shoppingCheckOut/ShoppingCheckOutInfo';
@@ -43,18 +45,38 @@ interface ShoppingCheckOutProps {
   cart: CartResponse;
 }
 
+const shippingSchema = yup.object().shape({
+  name: yup
+    .string()
+    .required('이름을 입력해주세요')
+    .min(2, '2자 이상 입력해주세요'),
+  phone: yup
+    .string()
+    .required('전화번호를 입력해주세요')
+    .matches(/^[0-9]{10,11}$/, '올바른 전화번호를 입력해주세요'),
+  address: yup.string().required('주소를 입력해주세요'),
+  city: yup.string().required('도/시를 입력해주세요'),
+  zipcode: yup
+    .string()
+    .required('우편번호를 입력해주세요')
+    .matches(/^\d{5}$/, '올바른 우편번호를 입력해주세요'),
+  memo: yup.string(),
+});
+
 export default function ShoppingCheckOut({
   onNextStep,
   cart,
 }: ShoppingCheckOutProps) {
-  const {
-    register,
-    handleSubmit,
-    formState: {errors},
-    watch,
-  } = useForm<ShippingFormData>({
+  const methods = useForm<ShippingFormData>({
+    resolver: yupResolver(shippingSchema) as Resolver<ShippingFormData>,
     mode: 'onChange',
   });
+
+  const {
+    formState: {errors},
+    watch,
+    handleSubmit,
+  } = methods;
 
   const [isFormError, setIsFormError] = useState(false);
 
@@ -65,7 +87,6 @@ export default function ShoppingCheckOut({
   const zipCode = watch('zipcode');
 
   const hasEmptyFields = !name || !phone || !address || !city || !zipCode;
-
   const hasFormErrors = !!(
     errors.name ||
     errors.phone ||
@@ -76,10 +97,8 @@ export default function ShoppingCheckOut({
 
   const onSubmit = async (data: ShippingFormData) => {
     try {
-      console.log(data);
       await postRequest(API_ENDPOINTS.SHIPPING_ADDRESS, data);
       console.log('배송 정보 저장 성공');
-
       setIsFormError(false);
     } catch (error) {
       console.error(error);
@@ -88,21 +107,18 @@ export default function ShoppingCheckOut({
   };
 
   return (
-    <Container>
-      <BillingContainer>
-        <BillingDetail>배송 정보</BillingDetail>
-        <ShoppingCheckOutInfo
-          onSubmit={onSubmit}
-          register={register}
-          errors={errors}
-          handleSubmit={handleSubmit}
+    <FormProvider {...methods}>
+      <Container>
+        <BillingContainer>
+          <BillingDetail>배송 정보</BillingDetail>
+          <ShoppingCheckOutInfo onSubmit={onSubmit} />
+        </BillingContainer>
+        <ShoppingCheckOutOrder
+          onNextStep={onNextStep}
+          cart={cart}
+          isFormError={isFormError || hasEmptyFields || hasFormErrors}
         />
-      </BillingContainer>
-      <ShoppingCheckOutOrder
-        onNextStep={onNextStep}
-        cart={cart}
-        isFormError={isFormError || hasEmptyFields || hasFormErrors}
-      />
-    </Container>
+      </Container>
+    </FormProvider>
   );
 }
