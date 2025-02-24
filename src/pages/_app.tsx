07dfa19ad4be7f33028487c5886302df31ff9349
JungMin <wjdals0304@gmail.com>
2025-02-24 20:38:43 +0900

    feat: react-toastify 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type {AppProps} from 'next/app';
 import '@/styles/reset.css';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ToastContainer} from 'react-toastify';
 
 export default function App({Component, pageProps}: AppProps) {
   const queryClient = new QueryClient();
@@ -8,6 +9,7 @@ export default function App({Component, pageProps}: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <ToastContainer />
     </QueryClientProvider>
   );
 }
