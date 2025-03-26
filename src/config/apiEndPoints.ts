a800a6849e33af3a85253003545af49e461228e1
JungMin <wjdals0304@gmail.com>
2025-03-26 11:49:53 +0900

    feat: vercel 환경 구축

--- src/config/apiEndPoints.ts
+++ src/config/apiEndPoints.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:3001';
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001';
 
 export const API_ENDPOINTS = {
   ROOT: '/',
